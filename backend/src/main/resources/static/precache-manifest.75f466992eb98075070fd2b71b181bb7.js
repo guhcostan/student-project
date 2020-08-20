c5dd4755fd8df1a4ff1ec91bf90da8a3b01c22f8
Gustavo Costa <gustavo.neves@equals.com.br>
2020-08-20 10:42:34 -0300

    changing name

---
+++ backend/src/main/resources/static/precache-manifest.75f466992eb98075070fd2b71b181bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fdb2dcdf902a627bd246e29692e4e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c98aa2f4dd673a32275",
+    "url": "/static/css/main.144a08bb.chunk.css"
+  },
+  {
+    "revision": "ec993d142bf2d3f78a5d",
+    "url": "/static/js/2.83e52a89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83e52a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c98aa2f4dd673a32275",
+    "url": "/static/js/main.df5447ef.chunk.js"
+  },
+  {
+    "revision": "6d4d1acb838a6c470c67",
+    "url": "/static/js/runtime-main.e602bd2d.js"
+  }
+]);
